51426009a93a6a131e56ce434e390443b6860a18
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-11-02 14:00:06 +0900

    Remove unused import

--- feature/post/src/main/java/com/conf/mad/todo/post/component/AddTaskTopAppBar.kt
+++ feature/post/src/main/java/com/conf/mad/todo/post/component/AddTaskTopAppBar.kt
@@ -1,7 +1,6 @@
 package com.conf.mad.todo.post.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
