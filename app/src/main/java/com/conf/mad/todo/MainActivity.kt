edd3082c1d4b2ebd94c84d4370dfe525d1d59f8b
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-11-02 15:58:07 +0900

    Set hilt in App

--- app/src/main/java/com/conf/mad/todo/MainActivity.kt
+++ app/src/main/java/com/conf/mad/todo/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.conf.mad.todo.designsystem.TodoTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
