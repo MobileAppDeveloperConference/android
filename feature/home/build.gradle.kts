04289d1eabb7ab4a0a96b3e4e557f2fd2028c728
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-11-02 16:21:13 +0900

    Add feature home module

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,16 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("conf.mad.convention.android.feature")
+}
+
+android {
+    namespace = "com.conf.mad.todo.home"
+
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+}
+
+dependencies {
+    implementation(projects.core.designsystem)
+}
