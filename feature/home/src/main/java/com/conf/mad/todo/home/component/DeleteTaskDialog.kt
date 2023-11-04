257e11fd1e3629df7b3e8a26ba20389c73f71f1f
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-11-05 01:36:47 +0900

    Change container color of dialog

--- feature/home/src/main/java/com/conf/mad/todo/home/component/DeleteTaskDialog.kt
+++ feature/home/src/main/java/com/conf/mad/todo/home/component/DeleteTaskDialog.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -33,7 +34,8 @@ fun DeleteTaskDialog(
                 .padding(16.dp)
                 .height(240.dp)
                 .fillMaxWidth(),
-            shape = RoundedCornerShape(4.dp)
+            shape = RoundedCornerShape(4.dp),
+            colors = CardDefaults.cardColors(containerColor = TodoTheme.colors.surface)
         ) {
             Column(
                 modifier = Modifier
