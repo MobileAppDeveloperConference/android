74a2e464141861f961ec4fe3e72b9ab98c101c35
HyunWoo Lee (Nunu Lee) <54518925+l2hyunwoo@users.noreply.github.com>
2023-11-05 01:52:23 +0900

    Set up status bar color transparent (#18)

--- app/src/main/java/com/conf/mad/todo/MainActivity.kt
+++ app/src/main/java/com/conf/mad/todo/MainActivity.kt
@@ -2,7 +2,10 @@ package com.conf.mad.todo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.DisposableEffect
 import com.conf.mad.todo.designsystem.TodoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,6 +14,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            DisposableEffect(true) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.light(
+                        android.graphics.Color.TRANSPARENT,
+                        android.graphics.Color.TRANSPARENT,
+                    ),
+                )
+                onDispose {}
+            }
             TodoTheme {
                 TodoApp()
             }
