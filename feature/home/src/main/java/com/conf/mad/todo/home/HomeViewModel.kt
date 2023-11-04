5b8dd3574e510cd9a2edf7380b553cc870abf88b
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-11-05 03:07:34 +0900

    Todo -> complete로 상태변경 시에 전환효과 추가

--- feature/home/src/main/java/com/conf/mad/todo/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/conf/mad/todo/home/HomeViewModel.kt
@@ -1,13 +1,16 @@
 package com.conf.mad.todo.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.conf.mad.todo.home.model.HomeMenu
 import com.conf.mad.todo.home.model.HomeUiState
+import com.conf.mad.todo.home.model.TaskStatus
 import com.conf.mad.todo.home.model.TaskUiModel
 import com.conf.mad.todo.task.repository.TaskRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
@@ -38,7 +41,13 @@ class HomeViewModel @Inject constructor(
                                 return@filter true
                             }
                             task.isFavorite
-                        }.map(TaskUiModel::of).toPersistentList(),
+                        }.map(TaskUiModel::of).map { task ->
+                            if (it.doneTasks.find { doneTask -> doneTask.id == task.id } != null) {
+                                task.copy(status = TaskStatus.DONE)
+                            } else {
+                                task
+                            }
+                        }.toPersistentList(),
                         completedTasks = completed.filter { task ->
                             if (!uiState.isOnlyFavoriteTaskVisible) {
                                 return@filter true
@@ -70,8 +79,24 @@ class HomeViewModel @Inject constructor(
     }
 
     fun onCompletedChanged(id: Long, isCompleted: Boolean) {
+        if (isCompleted) {
+            _uiState.update {
+                val task = it.todoTasks.find { task -> task.id == id } ?: return@update it
+                it.copy(
+                    doneTasks = (it.doneTasks + task).toPersistentList(),
+                )
+            }
+        }
         viewModelScope.launch {
+            if (isCompleted) {
+                delay(500)
+            }
             repository.updateCompleted(id, isCompleted)
+            _uiState.update {
+                it.copy(
+                    doneTasks = it.doneTasks.filter { task -> task.id != id }.toPersistentList()
+                )
+            }
         }
     }
 
