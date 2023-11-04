c97221da1d341fc71d24409f20b7e632b70a3530
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-11-04 19:31:16 +0900

    Define implementation of local data source

--- data/task/src/main/java/com/conf/mad/todo/data/task/datasource/TaskLocalDataSource.kt
+++ data/task/src/main/java/com/conf/mad/todo/data/task/datasource/TaskLocalDataSource.kt
@@ -1,10 +1,11 @@
 package com.conf.mad.todo.data.task.datasource
 
 import com.conf.mad.todo.model.Task
+import kotlinx.coroutines.flow.Flow
 
 interface TaskLocalDataSource {
-    fun getTodoTasks(): List<Task>
-    fun getCompletedTasks(): List<Task>
+    fun getTodoTasks(): Flow<List<Task>>
+    fun getCompletedTasks(): Flow<List<Task>>
     suspend fun insertTask(task: Task)
     suspend fun deleteTask(task: Task)
     suspend fun updateFavorite(id: Long, isFavorite: Boolean)
