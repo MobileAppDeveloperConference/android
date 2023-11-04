b3c584408a39e3846661a6d8d0029a77ecc80f10
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-11-05 02:20:13 +0900

    Fix alpha of background and add item in list

--- feature/home/src/main/java/com/conf/mad/todo/home/component/EmptyTaskView.kt
+++ feature/home/src/main/java/com/conf/mad/todo/home/component/EmptyTaskView.kt
@@ -3,6 +3,7 @@ package com.conf.mad.todo.home.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -28,6 +29,7 @@ internal fun EmptyTaskView(
             contentDescription = "Empty View",
             modifier = Modifier.padding(vertical = 18.dp)
         )
+        Spacer(modifier = Modifier.height(4.dp))
         Text(
             text = "투두를 추가해보세요",
             style = TodoTheme.typography.medium2,
@@ -42,11 +44,10 @@ private fun EmptyTaskPreview() {
     TodoTheme {
         EmptyTaskView(
             modifier = Modifier
-                .background(SkyBlue.copy(alpha = 0.5f))
+                .background(SkyBlue.copy(alpha = 0.3f))
                 .fillMaxWidth()
                 .padding(top = 20.dp)
                 .height(128.dp)
-
         )
     }
 }
